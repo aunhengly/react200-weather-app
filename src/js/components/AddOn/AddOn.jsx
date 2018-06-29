7e2dd1fed10087c6e62bca800328c389972299e6
Heng Ly Aun <aunhengly@gmail.com>
2018-06-29 10:06:11 -0700

    testing direct link to heroku

--- src/js/components/AddOn/AddOn.jsx
+++ src/js/components/AddOn/AddOn.jsx
@@ -1,33 +1,30 @@
 import React from 'react';
-import { searchGo } from '../Search/searchAction'
+import { searchGo } from '../Search/searchAction';
 // import { updateSearch, searchGo } from '../Search/searchAction';
-
 export default class AddOn extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(e) {
+    // console.log(this.props);
+    const { dispatch } = this.props;
+    dispatch(searchGo(e.target.name));
+  }
 
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick(e) {
-        // console.log(this.props);
-        const { dispatch } = this.props;
-        dispatch(searchGo(e.target.name));
-    }
-
-    render() {
-        return (
-            <div className='container'>
-                <div className="btn-group" role="group">
-                    {/* <button type="button" className="btn btn-primary btn-sm" onClick={()=>{this.handleClick("San Diego")}}>San Diego</button> */}
-                    <button type="button" name="San Diego" className="btn btn-primary btn-sm" onClick={this.handleClick}>San Diego</button>
-                    <button type="button" name="New York" className="btn btn-primary btn-sm" onClick={this.handleClick}>New York</button>
-                    <button type="button" name="Washington" className="btn btn-primary btn-sm" onClick={this.handleClick}>Washing D.C</button>
-                    <button type="button" name="London" className="btn btn-primary btn-sm" onClick={this.handleClick}>London</button>
-                    <button type="button" name="Tokyo" className="btn btn-primary btn-sm" onClick={this.handleClick}>Tokyo</button>
-                    <button type="button" name="Phnom Penh" className="btn btn-primary btn-sm" onClick={this.handleClick}>Phnom Penh</button>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className='container'>
+        <div className='btn-group' role='group'>
+          {/* <button type="button" className="btn btn-primary btn-sm" onClick={()=>{this.handleClick("San Diego")}}>San Diego</button> */}
+          <button type='button' name='San Diego' className='btn btn-primary btn-sm' onClick={ this.handleClick }>San Diego</button>
+          <button type='button' name='New York' className='btn btn-primary btn-sm' onClick={ this.handleClick }>New York</button>
+          <button type='button' name='Washington' className='btn btn-primary btn-sm' onClick={ this.handleClick }>Washing D.C</button>
+          <button type='button' name='London' className='btn btn-primary btn-sm' onClick={ this.handleClick }>London</button>
+          <button type='button' name='Tokyo' className='btn btn-primary btn-sm' onClick={ this.handleClick }>Tokyo</button>
+          <button type='button' name='Phnom Penh' className='btn btn-primary btn-sm' onClick={ this.handleClick }>Phnom Penh</button>
+        </div>
+      </div>
+    );
+  }
 }
